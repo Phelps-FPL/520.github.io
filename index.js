13c1f2a08790815cf16d4bd6c07813cdd6746107
phelps <phelps@phelpsdeMacBook-Pro.local>
2019-05-18 16:49:39 +0800

    updata

--- index.js
+++ index.js
@@ -52,7 +52,7 @@
       $('#style-tag').html(finalStyle);
       pre = document.getElementById('style-text');
       return pre.scrollTop = pre.scrollHeight;
-    }), 0 * commentTime);
+    }), 5000 * commentTime);
   };
 
   $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>跳过动画</a>\n    <a style='color: #fff' href='https://phelps-fpl.github.io/520.io/'>重新播放</a>\n    <a style='color: #fff' target=\"_blank\" href='https://github.com/idealclover/Love-Gift'>项目地址</a>\n  </div>");
@@ -67,6 +67,6 @@
     });
   });
 
-  writeStyles(styles, .1, commentTime);
+  writeStyles(styles, 1, commentTime);
 
 }).call(this);
